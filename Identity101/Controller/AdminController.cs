c6231bd7ac829e50f4a93025501bc91e06e2bf28
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-04-15 10:21:47 +0300

    Get User Api ok

--- Identity101/Controller/AdminController.cs
+++ Identity101/Controller/AdminController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
+
+using Identity101.Models.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity101.Controllers;
@@ -6,9 +10,30 @@ namespace Identity101.Controllers;
 [Authorize(Roles = "Admin")]
 public class AdminController : Controller
 {
-    //[AllowAnonymous]  = Admin de user de herkes bunu yapınca görebilir.  [Athorize] sadece login olmuş kişi görebilir.
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    
+    public AdminController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    //[AllowAnonymous]
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult AllUsers()
+    {
+        var users = _userManager.Users.Select(x => new
+            {
+                x.Id,
+                x.Name,
+                x.Surname,
+                x.Email
+            }).ToList();
+
+        return Ok(users);
     }
 }
