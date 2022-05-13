081ab612871f25cbf130b30156cf581c41aa36b9
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-05-13 12:32:58 +0300

    Service Extensions Added

--- AdminTemplate/Program.cs
+++ AdminTemplate/Program.cs
@@ -1,64 +1,15 @@
-using AdminTemplate.Data;
-using AdminTemplate.Models.Identity;
-using AdminTemplate.Services.Email;
-using Microsoft.AspNetCore.Identity;
+﻿using AdminTemplate.Data;
+using AdminTemplate.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
-using AdminTemplate.BusinessLogic.Repository;
-using AdminTemplate.BusinessLogic.Repository.Abstracts;
-using AdminTemplate.MappingProfiles;
-using AdminTemplate.Models.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 var con1 = builder.Configuration.GetConnectionString("con1");
 builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(con1));
 
-#region Identity
 
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 3;
-    options.Lockout.AllowedForNewUsers = false;
-
-    // User settings.
-    options.User.AllowedUserNameCharacters =
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
-    options.User.RequireUniqueEmail = true;
-}).AddEntityFrameworkStores<MyContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    // Cookie settings
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-
-    options.LoginPath = "/Account/Login";
-    options.AccessDeniedPath = "/Account/AccessDenied";
-    options.SlidingExpiration = true;
-});
-
-#endregion
-
-builder.Services.AddTransient<IEmailService, SmtpEmailService>();
-
-#region Repositories
-
-builder.Services.AddScoped<IRepository<Product, Guid>, ProductRepo>();
-builder.Services.AddScoped<IRepository<Category, int>, CategoryRepo>();
-
-#endregion
-
+//ServiceExtensions classýnda tanýmladýðýmýz servisleri 1 kerede aktardýk.
+builder.Services.AddServices();
 
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation()
     .AddJsonOptions(options =>
@@ -66,11 +17,6 @@ builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation()
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     });
 
-//builder.Services.AddAutoMapper(options => options.AddMaps("AdminTemplate.MappingProfiles"));
-builder.Services.AddAutoMapper(options =>
-{
-    options.AddProfile<EntityMappingProfile>();
-});
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
