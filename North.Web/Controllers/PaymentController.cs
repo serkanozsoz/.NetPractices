deab48051206474586fcd59de24a5e2b7248027e
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-06-02 15:00:04 +0300

    Payment, Cart Added

---
+++ North.Web/Controllers/PaymentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using North.Businesss.Services.Payment;
+
+namespace North.Web.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Route("guvenli-odeme")]
+        public IActionResult Pay()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("taksit-kontrol")]
+        public IActionResult CheckInstallments(string cardNumber, decimal price)
+        {
+            if (string.IsNullOrEmpty(cardNumber))
+                return BadRequest();
+            var result = _paymentService.CheckInstallments(cardNumber, price);
+            return Ok(result);
+        }
+
+    }
+}
